cc1e003bb2eb57947ed0a8395ac79f1ec5e8e721
Jim Hill <jim.hill.r@gmail.com>
2020-03-21 21:47:04 -0500

    Added s3 bucket to save training to

--- frontend/src/app/src/pages/Training.vue
+++ frontend/src/app/src/pages/Training.vue
@@ -1,18 +1,30 @@
 <template>
-  <q-page padding>
-    <div class="row justify-center">
-      <div class="text-center">
-        <p>
-          <q-btn color="accent" glossy :label="recordLabel" @click="record()" /> &nbsp;
-          <q-btn color="accent" glossy label="Replay" @click="replay()" /> &nbsp;
-          <q-btn color="info" glossy label="Clear" @click="clear()" />
-        </p>
+  <div>
+    <q-page padding>
+      <div class="row justify-center">
+        <div class="text-center">
+          <p>
+            <q-btn color="accent" glossy :label="recordLabel" @click="record()" /> &nbsp;
+            <q-btn color="accent" glossy label="Replay" @click="replay()" /> &nbsp;
+            <q-btn color="info" glossy label="Clear" @click="clear()" /> &nbsp;
+            <q-btn v-if="recording.length > 0" color="green" glossy label="Save" @click="showDialog = true" />
+          </p>
+        </div>
       </div>
-    </div>
-    <div class="row justify-center">
-        <EelCanvas ref="whiteboard"></EelCanvas>
-    </div>
-  </q-page>
+      <div class="row justify-center">
+          <EelCanvas ref="whiteboard"></EelCanvas>
+      </div>
+    </q-page>
+    <q-dialog v-model="showDialog">
+      <q-card>
+        <q-card-section>
+          <q-select outlined v-model="letterTrained" :options="letterOptions" label="Letter" />
+          <q-select outlined v-model="letterQuality" :options="qualityOptions" label="Quality" />
+          <q-btn color="green" glossy label="Save" @click="save()" />
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </div>
 </template>
 
 <script>
@@ -25,7 +37,13 @@ export default {
   },
   data () {
     return {
-      recordLabel: 'Record'
+      recording: [],
+      recordLabel: 'Record',
+      letterTrained: 'a',
+      letterQuality: 'Excellent',
+      showDialog: false,
+      letterOptions: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+      qualityOptions: ['Excellent', 'Good', 'Okay', 'Poor', 'Egregious']
     }
   },
   methods: {
@@ -47,6 +65,33 @@ export default {
     },
     clear () {
       this.$refs.whiteboard.clear()
+    },
+    save () {
+      this.showDialog = false
+      if (this.letterTrained.length !== 1) {
+        return
+      }
+      let now = new Date()
+      var timestamp = Date.UTC(
+        now.getUTCFullYear(),
+        now.getUTCMonth(),
+        now.getUTCDate(),
+        now.getUTCHours(),
+        now.getUTCMinutes(),
+        now.getUTCSeconds())
+      let data = {
+        letter: this.letterTrained,
+        quality: this.letterQuality,
+        path: this.recording
+      }
+      let config = {
+        headers: {
+          'content-type': 'application/json',
+          'x-amz-acl': 'public-read'
+        }
+      }
+      let filename = `${timestamp}.json`
+      this.$axios.put(`https://eel3-data.s3.us-east-2.amazonaws.com/patterns/${this.letterTrained}/${filename}`, data, config)
     }
   }
 }
