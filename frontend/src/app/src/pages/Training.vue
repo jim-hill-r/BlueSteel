17b6e214ff3caae297934f848a940b0e4d959711
Jim Hill <jim.hill.r@gmail.com>
2020-03-23 16:59:24 -0500

    Refactored training page to match practice pages

--- frontend/src/app/src/pages/Training.vue
+++ frontend/src/app/src/pages/Training.vue
@@ -1,26 +1,38 @@
 <template>
   <div>
     <q-page padding>
-      <div class="row justify-center">
-        <div class="text-center">
-          <p>
-            <q-btn color="accent" glossy :label="recordLabel" @click="record()" /> &nbsp;
-            <q-btn color="accent" glossy label="Replay" @click="replay()" /> &nbsp;
-            <q-btn color="info" glossy label="Clear" @click="clear()" /> &nbsp;
-            <q-btn v-if="recording.length > 0" color="green" glossy label="Save" @click="showDialog = true" />
-          </p>
+      <div>
+        <div class="row justify-center">
+          <div class="text-center">
+            <q-card class="my-card">
+              <q-card-section class="bg-accent text-white">
+                <div class="text-h6">{{instructions}}</div>
+                <div class="text-subtitle2">{{subtext}}</div>
+              </q-card-section>
+
+              <q-card-actions align="around">
+                <q-btn v-if="step === 'start'" color="accent" flat label="Record" @click="record()"></q-btn>
+                <q-btn v-if="step === 'record'" color="negative" flat label="Stop" @click="stop()"></q-btn>
+                <q-btn v-if="step === 'save'" color="info" flat label="Replay" @click="replay()"></q-btn>
+                <q-btn v-if="step === 'save'" color="positive" flat label="Save" @click="save()"></q-btn>
+                <q-btn v-if="step === 'save'" color="info" flat label="Restart" @click="restart()"></q-btn>
+              </q-card-actions>
+            </q-card>
+            <q-card class="my-card">
+              <q-card-section>
+                <EelCanvas :active="isCanvasActive" ref="whiteboard"></EelCanvas>
+              </q-card-section>
+            </q-card>
+          </div>
         </div>
-      </div>
-      <div class="row justify-center">
-          <EelCanvas ref="whiteboard"></EelCanvas>
       </div>
     </q-page>
     <q-dialog v-model="showDialog">
       <q-card>
         <q-card-section>
-          <q-select outlined v-model="letterTrained" :options="letterOptions" label="Letter" />
-          <q-select outlined v-model="letterQuality" :options="qualityOptions" label="Quality" />
-          <q-btn color="green" glossy label="Save" @click="save()" />
+          <q-select outlined v-model="letter" :options="letterOptions" label="Letter" />
+          <q-select outlined v-model="quality" :options="qualityOptions" label="Quality" />
+          <q-btn color="positive" glossy label="Upload" @click="upload()" />
         </q-card-section>
       </q-card>
     </q-dialog>
@@ -37,10 +49,13 @@ export default {
   },
   data () {
     return {
+      step: 'start',
+      instructions: 'Welcome',
+      subtext: 'Click record when you are ready to begin.',
+      isCanvasActive: false,
       recording: [],
-      recordLabel: 'Record',
-      letterTrained: 'a',
-      letterQuality: 'Excellent',
+      letter: 'a',
+      quality: 'Excellent',
       showDialog: false,
       letterOptions: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
       qualityOptions: ['Excellent', 'Good', 'Okay', 'Poor', 'Egregious']
@@ -48,31 +63,48 @@ export default {
   },
   methods: {
     record () {
-      if (this.isRecording) {
-        this.isRecording = false
-        this.recordLabel = 'Record'
-        this.recording = this.$refs.whiteboard.getRecording()
-      } else {
-        this.isRecording = true
-        this.recordLabel = 'Stop'
-        this.$refs.whiteboard.record()
-      }
+      this.$refs.whiteboard.record()
+      this.isCanvasActive = true
+      this.instructions = 'Draw a lowercase letter'
+      this.subtext = 'The slower your pace, the more data will be collected.'
+      this.step = 'record'
+    },
+    stop () {
+      this.recording = this.$refs.whiteboard.getRecording()
+      this.isCanvasActive = false
+      this.instructions = 'Review your recording'
+      this.subtext = 'Press replay to see how this will show to students.'
+      this.step = 'save'
     },
     replay () {
-      this.clear()
-      this.$refs.whiteboard.draw(this.recording, true)
-    },
-    clear () {
       this.$refs.whiteboard.clear()
+      this.instructions = 'Replaying...'
+      this.subtext = '...'
+      this.$refs.whiteboard.draw(this.recording, true)
+      setTimeout(() => {
+        this.instructions = 'Save your recording'
+        this.subtext = 'Press save to upload your recording to the database.'
+      }, 3000)
     },
     save () {
-      this.showDialog = false
+      this.showDialog = true
+    },
+    restart () {
+      this.$refs.whiteboard.clear()
+      this.instructions = 'Begin when ready'
+      this.subtext = 'Click record when you are ready to begin.'
+      this.step = 'start'
+    },
+    upload () {
       let pattern = {
-        letter: this.letterTrained,
-        quality: this.letterQuality,
+        letter: this.letter,
+        quality: this.quality,
         path: this.recording
       }
       this.$store.dispatch('common/uploadPattern', pattern)
+      this.instructions = 'Saved'
+      this.subtext = 'Press restart to begin again.'
+      this.showDialog = false
     }
   }
 }
