83966250549eab101efe9a5487e91fb70791f114
Jim Hill <jim.hill.r@gmail.com>
2020-04-28 17:54:53 -0500

    Added endpoint checks to ensure directionality is met

--- frontend/src/client/src/logic/validate.js
+++ frontend/src/client/src/logic/validate.js
@@ -1,6 +1,13 @@
 import * as path from './path.js'
+import * as vector from './vector.js'
 
 export function compare (a, b) {
+  return comparePathLength(a, b) &&
+    comparePoints(a, b, 'start') &&
+    comparePoints(a, b, 'end')
+}
+
+function comparePathLength (a, b) {
   const countA = a.length || 0
   const countB = b.length || 0
   if (countA < 10 || countB < 10) {
@@ -18,4 +25,24 @@ export function compare (a, b) {
     return true
   }
   return false
+}
+
+function comparePoints (a, b, typeFilter) {
+  let filteredA = a.filter(point => point.type === typeFilter)
+  let filteredB = b.filter(point => point.type === typeFilter)
+
+  let size = path.size(a)
+  for (let i = 0; i < filteredA.length; i++) {
+    let minDist = size
+    for (let j = 0; j < filteredB.length; j++) {
+      let lineAB = vector.substract(filteredA[i], filteredB[j])
+      let dist = vector.magnitude(lineAB)
+      minDist = Math.min(minDist, dist)
+    }
+    let error = minDist / size
+    if (error > 0.1) {
+      return false
+    }
+  }
+  return true
 }
